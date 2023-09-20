622716a9061f8aa71dfaf98fb14a9aaa6e5ea35b
maddadder <rleecharlie@gmail.com>
2023-09-19 19:05:02 -0700

    just change it to comic sans ms

--- chrome_extension/content_script.js
+++ chrome_extension/content_script.js
@@ -1,21 +1,24 @@
 function boldText(node) {
     const regex = /(\b[a-zA-Z]{1,3}\b|\b[a-zA-Z]{4,7}\b|\b[a-zA-Z]{8,}\b)/g;
     const newText = node.textContent.replace(regex, (word) => {
+      /*
       const length = word.length;
       let boldLength;
   
       if (length <= 3) {
         boldLength = 1;
-      } else if (length <= 7) {
-        boldLength = 2;
-      } else {
+      } else if (length <= 5) {
         boldLength = 3;
+      } else if (length <= 7) {
+        boldLength = 5;
+      } else {
+        boldLength = 7;
       }
   
       const boldPart = word.slice(0, boldLength);
       const remainingPart = word.slice(boldLength);
-  
-      return `<b>${boldPart}</b>${remainingPart}`;
+      */
+      return `<span style="font-size: x-large;font-family:'comic sans ms';">${word}</span>`;
     });
   
     const newNode = document.createElement("span");
